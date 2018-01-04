313eca0354539f4b1c3878ae129123796e6ce7ea
iosdevlog <iosdevlog@iosdevlog.com>
2018-01-05 00:13:19 +0800

    First Commit!

---
+++ app/src/main/java/iosdevlog/com/jump/JumpActivityFragment.kt
@@ -0,0 +1,18 @@
+package iosdevlog.com.jump
+
+import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+class JumpActivityFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_jump, container, false)
+    }
+}
